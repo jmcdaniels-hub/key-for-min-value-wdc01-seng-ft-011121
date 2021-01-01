e4099ac9018572471fc923371b403130cf0da8c0
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 22:28:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+#{:blake => 10, :ashley => 50, :adam => 17}
 
 def key_for_min_value(name_hash)
-
+min_value = 0
+index = 0
+  
+  name_hash.each do |key, value|
+    
+    if value[index] < value[index + 1]
+      min_value = value[index]
+      index +=1 
+    end   
+  end 
+  
 end
