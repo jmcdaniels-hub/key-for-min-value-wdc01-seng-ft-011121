5805367a3d642490336579e19f71f08f6355905d
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 22:48:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,15 @@
 #{:blake => 10, :ashley => 50, :adam => 17}
 
 def key_for_min_value(name_hash)
-
+min_value = 0 
 row_index = 0
 
 name_array = name_hash.to_a
   
   name_array.each do 
     element_index = 1 
-    if name_array[row_index][element_index]
+    if name_array[row_index][element_index] < name_array[row_index][element_index]
+      min_value = name_array[row_index][element_index]
     end   
   end 
 end
