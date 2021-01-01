34b628ed5383794b9185a67c4ef2acebc35e7745
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 22:28:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,10 @@
 #{:blake => 10, :ashley => 50, :adam => 17}
 
 def key_for_min_value(name_hash)
-min_value = 0
-index = 0
+#min_value = 0
+#index = 0
+
+name_hash.to_a
   
   name_hash.each do |key, value|
     
